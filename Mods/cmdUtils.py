4be4a43e64248ea4fa8add667b77e71109e28276
Tomo-Nyan <tomo@katter.club>
2018-07-14 17:29:26 +0100

    *actually* added cmdUtils.py

---
+++ Mods/cmdUtils.py
@@ -0,0 +1,32 @@
+#Module for miscellaneous functions.
+
+def concat(array,*args):
+    if len(args) < 1:
+        type = str
+    else:
+        type = args[0]
+    convArray = map(type,array)
+    concat = "".join(convArray)
+    return concat
+
+def userInDB(userID,connection):
+    with connection.cursor() as cursor:
+        cursor.execute(concat(("SELECT userID FROM tblUser WHERE userID = ",userID,";")))
+        if cursor.fetchone() == None:
+            return False
+        else:
+            return True
+
+def guildInDB(serverID,connection):
+    with connection.cursor() as cursor:
+        cursor.execute(concat(("SELECT serverID FROM tblServerPrefixes WHERE serverID = '",serverID,"';")))
+        if cursor.fetchone() == None:
+            return False
+        else:
+            return True
+
+def userExists(userID):
+    if userID.getUser() == None:
+        return False
+    else:
+        return True
