7042e52af4109a9e5f033d10918e5126010624d7
Tomo-Nyan <tomo@katter.club>
2018-07-20 23:30:23 +0100

    fixed debug code.. I am an idiot sandwich

--- robot.py
+++ robot.py
@@ -355,9 +355,8 @@ async def on_message(message):
     elif message.content == command("leaderboard global", message):
         scoreList = ""
         globalScores = trivia.getGlobalLeaderboard(connection)
-        print(utils.concat(("[DEBUG] globalScores = ",globalScores)))
-        print(utils.concat(("[DEBUG] globalScores[1] = ",globalScores)))
-        print(utils.concat(("[DEBUG] globalScores[0] = ",globalScores)))
+        print(utils.concat(("[DEBUG] globalScores[1] = ",globalScores.getScore())))
+        print(utils.concat(("[DEBUG] globalScores[0] = ",globalScores.getScore())))
         if len(globalScores) < 10:
             place = 1
             for score in globalScores:
